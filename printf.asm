850239e3ed559a44aca549e11911bc6b3c86d68e
apselon <ildarovadam@gmail.com>
2020-03-30 11:38:15 +0300

    fixed formating

--- printf.asm
+++ printf.asm
@@ -1,29 +1,29 @@
 %macro pushaq 0
-    	push rax 
-    	push rcx 
-    	push rdx 
-    	push rbx 
-    	push rbp 
-    	push rdi 
-    	push rsi 
-    	push r8
-    	push r9
+		push rax 
+		push rcx 
+		push rdx 
+		push rbx 
+		push rbp 
+		push rdi 
+		push rsi 
+		push r8
+		push r9
 		push r10 
 		push r11 
 %endmacro
 
 %macro popaq 0
-        pop r11 
-        pop r10 
-        pop r9
-        pop r8
-        pop rsi 
-        pop rdi 
-        pop rbp 
-        pop rbx 
-        pop rdx 
-        pop rcx 
-        pop rax 
+		pop r11 
+		pop r10 
+		pop r9
+		pop r8
+		pop rsi 
+		pop rdi 
+		pop rbp 
+		pop rbx 
+		pop rdx 
+		pop rcx 
+		pop rax 
 %endmacro
 
 section .data
